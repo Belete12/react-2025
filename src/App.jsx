9c59fd4f42207e4eab26e9fbf001332e071f0637
Belete <beleten143@gmail.com>
2025-12-09 21:01:31 -0500

    week03 assignment completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import {useState} from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
 
 function App() {
   const todos = [
@@ -7,14 +11,14 @@ function App() {
     { id: 3, title: "code out app" },
   ];
 
+  const [newTodo, setNewTodo] = useState("Todo list Items")
+
   return (
     <div>
       <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <TodoForm />
+      <p>{newTodo}</p>
+      <TodoList todos={todos} />
     </div>
   );
 }
---
+++ src/TodoForm.jsx
@@ -0,0 +1,14 @@
+
+function TodoForm(){
+
+    return(
+        <form>
+      <label> Todo:
+        <input type="text"  />
+      </label>
+        <button type="submit"> Add Todo</button>
+
+        </form>
+    )
+}
+export default TodoForm;
