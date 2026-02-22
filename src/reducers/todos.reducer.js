a5b8334871930cf6260e52821a5549ee56826326
Belete1117 <beleten143@gmail.com>
2026-02-21 21:51:19 -0500

    completed

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -2,19 +2,29 @@ const actions = {
   //actions in useEffect that loads todos
   fetchTodos: 'fetchTodos',
   loadTodos: 'loadTodos',
+
   //found in useEffect and addTodo to handle failed requests
   setLoadError: 'setLoadError',
+
   //actions found in addTodo
   startRequest: 'startRequest',
   addTodo: 'addTodo',
   endRequest: 'endRequest',
+
   //found in helper functions
   updateTodo: 'updateTodo',
   completeTodo: 'completeTodo',
+
   //reverts todos when requests fail
   revertTodo: 'revertTodo',
+
   //action on Dismiss Error button
   clearError: 'clearError',
+
+  //
+  setSortDirection: 'setSortDirection',
+  setSortField: 'setSortField',
+  setQueryString: 'setQueryString',
 };
 
 const initialState = {
@@ -22,6 +32,11 @@ const initialState = {
   isLoading: false,
   isSaving: false,
   errorMessage: '',
+
+  //
+  sortDirection: 'desc',
+  sortField: 'createdTime',
+  queryString: '',
 };
 
 function reducer(state = initialState, action) {
@@ -29,31 +44,115 @@ function reducer(state = initialState, action) {
     case actions.fetchTodos:
       return {
         ...state,
+        isLoading: true,
       };
+
     case actions.loadTodos:
       return {
         ...state,
+        todoList: action.records.map((record) => {
+          const todo = { id: record.id, ...record.fields };
+          if (!todo.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        }),
+        isLoading: false,
       };
-    //code continues...
+
     case actions.setLoadError:
-      return { ...state };
+      return {
+        ...state,
+        errorMessage: action.error.message,
+        isLoading: false,
+      };
+
     case actions.startRequest:
-      return { ...state };
-    case actions.addTodo:
-      return { ...state };
+      return {
+        ...state,
+        isSaving: true,
+      };
+
+    case actions.addTodo: {
+      const savedTodo = {
+        ...action.record,
+        isCompleted: action.record.isCompleted ?? false,
+      };
+      return {
+        ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
+      };
+    }
+
     case actions.endRequest:
-      return { ...state };
-    case actions.updateTodo:
-      return { ...state };
-    case actions.completeTodo:
-      return { ...state };
+      return {
+        ...state,
+        isLoading: false,
+        isSaving: false,
+      };
+
+    //make sure that the revertTodo case is written directly above updateTodo and remove the return statement.
     case actions.revertTodo:
-      return { ...state };
+    // no return
+    case actions.updateTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.editedTodo.id ? action.editedTodo : todo
+      );
+      const updatedState = {
+        ...state,
+        todoList: updatedTodos,
+      };
+      if (action.error) {
+        updatedState.errorMessage = action.error.message;
+      }
+      return updatedState;
+    }
+
+    case actions.completeTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.id
+          ? {
+              ...todo,
+              isCompleted: true,
+            }
+          : todo
+      );
+
+      return {
+        ...state,
+        todoList: updatedTodos,
+      };
+    }
+
     case actions.clearError:
-      return { ...state };
+      return {
+        ...state,
+        errorMessage: '',
+      };
+
+    // add
+    case actions.setSortDirection:
+      return {
+        ...state,
+        sortDirection: action.sortDirection,
+      };
+
+    case actions.setSortField:
+      return {
+        ...state,
+        sortField: action.sortField,
+      };
+
+    case actions.setQueryString:
+      return {
+        ...state,
+        queryString: action.queryString,
+      };
+
     default:
       return state;
   }
 }
 
-export { initialState, actions };
+export { initialState, actions, reducer };
